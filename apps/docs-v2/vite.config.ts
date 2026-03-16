c2d1689610e589fd3946d77328e500e9397244bd
lakubuDavid <Lakubudavid@gmail.com>
2026-03-16 14:57:30 +0400

    refact

--- apps/docs-v2/vite.config.ts
+++ apps/docs-v2/vite.config.ts
@@ -1,8 +1,8 @@
-import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import mdx from 'fumadocs-mdx/vite';
-import * as MdxConfig from './source.config';
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import mdx from "fumadocs-mdx/vite";
+import * as MdxConfig from "./source.config";
 
 export default defineConfig({
   plugins: [mdx(MdxConfig), tailwindcss(), reactRouter()],
